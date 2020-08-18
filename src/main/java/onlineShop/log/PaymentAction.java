2b5895487cf89e5215ba7c163d461300f993e3fa
glorianachen <57929370+glorianachen@users.noreply.github.com>
2020-08-17 22:51:13 -0400

    Initial commit

---
+++ src/main/java/onlineShop/log/PaymentAction.java
@@ -0,0 +1,15 @@
+package onlineShop.log;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PaymentAction {
+	@Autowired
+	private Logger logger;
+
+	public void pay(BigDecimal payValue) {
+		logger.log("pay begin, payValue is " + payValue);
+		logger.log("pay end");
+	}
+}
